fa97497bff5e0bcab9207f692a669fc19a31538d
KennaTeklu <140216776+KennaTeklu@users.noreply.github.com>
2026-05-10 17:48:10 -0700

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,5 +2,4 @@ import { defineConfig } from 'astro/config';
 
 export default defineConfig({
   site: 'https://kennateklu.github.io',
-  base: '/Game', // change if repo name is different, else remove
 });
