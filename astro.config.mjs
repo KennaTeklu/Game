3307664422eae2a64cfd28ba4f20109231efd0e0
KennaTeklu <140216776+KennaTeklu@users.noreply.github.com>
2026-05-10 17:34:13 -0700

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,5 +2,5 @@ import { defineConfig } from 'astro/config';
 
 export default defineConfig({
   site: 'https://kennateklu.github.io',
-  base: '/3d-tetris', // change if repo name is different, else remove
+  base: '/Game', // change if repo name is different, else remove
 });
